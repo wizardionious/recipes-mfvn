45184ebb12c2c297143ca3ee9bc27a3801f9da39
Artem Levchenko <levchencode@gmail.com>
2026-03-26 22:05:35 +0200

    some utils

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,29 @@
 export * from "./auth.schema.js";
 export * from "./category.schema.js";
 export * from "./recipe.schema.js";
+
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+export type RenameField<T extends object, K extends keyof T, N extends PropertyKey> = Omit<T, K> &
+  Record<N, T[K]>;
+
+export function renameField<T extends object, K extends keyof T, N extends PropertyKey>(
+  obj: T,
+  oldName: K,
+  newName: N,
+) {
+  const { [oldName]: value, ...rest } = obj;
+  return {
+    ...rest,
+    [newName]: value,
+  } as RenameField<T, K, N>;
+}
+
+export type OmitField<T extends object, K extends keyof T> = Omit<T, K>;
+
+export function omitField<T extends object, K extends keyof T>(obj: T, name: K): OmitField<T, K> {
+  const { [name]: _, ...rest } = obj;
+  return rest as OmitField<T, K>;
+}
