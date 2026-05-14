74769c760047cadcc60207552babbebaad2dc8b0
Andrii <wizardionious.education@gmail.com>
2026-05-14 20:36:08 +0300

    chore: button component

---
+++ apps/frontend/src/components/ui/AppButton.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup></script>
+
+<template>
+  <button type="button" class="button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  border: none;
+  background-color: transparent;
+  color: #555555;
+  cursor: pointer;
+  border-radius: 6px;
+  padding: 0.25rem 0.5rem;
+  gap: 8px;
+
+  &:has(> svg) {
+    padding: 0.5rem;
+    border-radius: 9999px;
+  }
+
+  &:hover {
+    background-color: #f2f2f2;
+  }
+}
+</style>
