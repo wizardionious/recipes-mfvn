a64d8e2db2a9636c78b20b2f703448b4d8c3be9e
Andrii <wizardionious.education@gmail.com>
2026-05-25 21:26:36 +0300

    feat(router): scroll behavior for anchors

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -16,6 +16,23 @@ const router = createRouter({
       component: RecipeDetailsView,
     },
   ],
+  scrollBehavior(to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: 72,
+        behavior: "smooth",
+      };
+    }
+
+    return {
+      top: 0,
+    };
+  },
 });
 
 export default router;
